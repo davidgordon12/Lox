a57007a71912d1ddff30a6da6a61d64d671630c5
tehtko <moderndsnsam@gmail.com>
2023-08-19 17:03:20 -0400

    Finish implementing basic lexer methods

--- src/lexer/lexer.go
+++ src/lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "lox/token"
+
 type Lexer struct {
 	input        string // Lox source code
 	position     int    // Current position in our input
@@ -7,7 +9,7 @@ type Lexer struct {
 	ch           byte   // The current character under examination
 
 	/*
-	* The reason for these two “pointers”
+	* The reason for these two “pointers” (position, readPosition)
 	* pointing into our input string is the fact that we will need to be able to “peek” further into
 	* the input and look after the current character to see what comes up next. readPosition always
 	* points to the “next” character in the input. position points to the character in the input that
@@ -17,10 +19,15 @@ type Lexer struct {
 
 func New(input string) *Lexer {
 	lexer := &Lexer{input: input}
+	lexer.ReadChar()
 	return lexer
 }
 
-func ReadChar(lexer *Lexer) {
+func NewToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func (lexer *Lexer) ReadChar() {
 	if lexer.readPosition >= len(lexer.input) {
 		lexer.ch = 0
 	} else {
@@ -31,5 +38,32 @@ func ReadChar(lexer *Lexer) {
 	lexer.readPosition += 1
 }
 
-func NextToken(lexer *Lexer) {
+func (lexer *Lexer) NextToken() token.Token {
+	var tkn token.Token
+
+	switch lexer.ch {
+	case '=':
+		tkn = NewToken(token.ASSIGN, lexer.ch)
+	case ';':
+		tkn = NewToken(token.SEMICOLON, lexer.ch)
+	case '(':
+		tkn = NewToken(token.LPAREN, lexer.ch)
+	case ')':
+		tkn = NewToken(token.RPAREN, lexer.ch)
+	case ',':
+		tkn = NewToken(token.COMMA, lexer.ch)
+	case '+':
+		tkn = NewToken(token.PLUS, lexer.ch)
+	case '{':
+		tkn = NewToken(token.LBRACE, lexer.ch)
+	case '}':
+		tkn = NewToken(token.RBRACE, lexer.ch)
+	case 0:
+		tkn.Literal = ""
+		tkn.Type = token.EOF
+	}
+
+	lexer.ReadChar()
+
+	return tkn
 }
