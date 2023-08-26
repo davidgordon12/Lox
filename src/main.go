a90042cf3ff421b461a183c8bc12da9d832f20c2
tehtko <moderndsnsam@gmail.com>
2023-08-26 15:37:04 -0400

    Start REPL

--- src/main.go
+++ src/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"lox/repl"
+	"os"
+)
 
 func main() {
-	fmt.Println("LOX")
+	fmt.Println("LOX Version 1.0")
+	fmt.Println("Enter 'help' to view available commands")
+
+	repl.Start(os.Stdin, os.Stdout)
 }
